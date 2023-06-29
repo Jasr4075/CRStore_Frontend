354e512f746d2c03d70389b3702ba856e902c731
parxivall <jasr4075@gmail.com>
2023-06-29 17:23:04 -0300

    jajsaj

---
+++ plugins/axios.js
@@ -0,0 +1,14 @@
+export default function ({ $axios }, inject) {
+  
+
+  const api = $axios.create()
+  
+  api.onRequest(() =>{
+    const token = localStorage.getItem('forget-key') || '';
+    api.setHeader('Authorization', `Bearer ${token}`)
+  })
+
+  api.setBaseURL('http://localhost:3333')
+
+  inject('api', api)
+}
